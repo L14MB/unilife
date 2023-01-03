4c4e0dfc96271d2ef619577672e364eca66d29cd
L14MB <liam6914@outlook.com>
2023-01-03 10:53:30 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+function App() {
+  const [email,setEmail]=useState('')
+  
+  
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    console.log(email)
+  }
+  
+    return (
+      <div>
+         <form onSubmit={handleSubmit}>
+          <input
+            onChange={(e)=>setEmail(e.target.value)}
+          />
+         </form>
+      </div>
+    );
+  }
+  
+  export default App;
